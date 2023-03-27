b54941a7310042b1298a8ca989fa7561a64d3aca
Matheus <nakashi00@live.com>
2023-03-27 15:05:02 -0300

    Creating the first class

--- ChessGame_Project/ChessGame_Project/Program.cs
+++ ChessGame_Project/ChessGame_Project/Program.cs
@@ -1,12 +1,15 @@
 ﻿
+using board;
+
 namespace ChessGame_Project
 {
     class Program
     {
         static void Main(string[] args) 
         {
+            Position P = new Position(3, 4);
 
-
+            Console.WriteLine(P);
         }
 
     }
---
+++ ChessGame_Project/ChessGame_Project/board/Position.cs
@@ -0,0 +1,20 @@
+﻿
+namespace board
+{
+    internal class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            this.Line = line;
+            this.Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line + ", " + Column;
+        }
+    }
+}
