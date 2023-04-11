e948771929768f4f96ac84437d53e58fb4581487
Matheus <nakashi00@live.com>
2023-04-11 11:14:25 -0300

    Created the ChessPosition class

--- ChessGame_Project/ChessGame_Project/Program.cs
+++ ChessGame_Project/ChessGame_Project/Program.cs
@@ -10,12 +10,8 @@ namespace ChessGame_Project
         {
             try
             {
-                Board brd = new Board(8, 8);
-
-                brd.insertPiece(new King(brd, Color.White), new Position(0, 0));
-                brd.insertPiece(new King(brd, Color.Black), new Position(0, 7));
-
-                Screen.printBoard(brd);
+                ChessPosition chessPosition = new ChessPosition('a', 2);
+                Console.WriteLine(chessPosition.toPosition());
             }
             catch (BoardException e)
             {
